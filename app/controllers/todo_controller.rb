1d26d1bfe9b575d265ab0bd4ce37ee305b62a9ca
Joshua Velazquez <joshua.velazquez@epicacademy.org>
2017-10-21 18:46:34 +0000

    todo_backend

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -2,7 +2,16 @@ class TodoController <ApplicationController
     def index
     end
     def show
-        @todo_description="Make The Curriculum"
-        @todo_number=4
+        todo_id=params[:id]
+        if todo_id=="1"
+         @todo_description="Make The Curriculum"
+         @todo_number=4
+         elsif todo_id=="2"
+         @todo_description="Buy workshop supplies"
+         @todo_number=3
+        elsif todo_id=="3"
+        @todo_description="Meet with the volunteer trainers"
+        @todo_number="2"
+        end
     end
 end
