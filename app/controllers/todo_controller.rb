2f87603960fe4e33b98004e2eb609ab21656a9a7
Joshua Velazquez <joshua.velazquez@epicacademy.org>
2017-10-22 16:41:22 +0000

    Todo_backend

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -2,28 +2,15 @@ class TodoController <ApplicationController
     def index
     end
     def show
-        todo_id=params[:id]
-        if todo_id=="1"
-         @todo_description="Make The Curriculum"
-         @todo_number=4
-         elsif todo_id=="2"
-         @todo_description="Buy workshop supplies"
-         @todo_number=3
-        elsif todo_id=="3"
-        @todo_description="Meet with the volunteer trainers"
-        @todo_number="2"
-        elsif todo_id=="4"
-        @todo_description="Order food for Saturday and Sunday"
-        @todo_number="1"
-        elsif todo_id=="5"
-        @todo_description="Check pre-work assignments"
-        @todo_number="2"
-        elsif todo_id=="6"
-        @todo_description="Send workshop location to all the students"
-        @todo_number="1"
-        elsif todo_id=="7"
-        @todo_description="Have a great workshop!!!"
-        @todo_number="0"
-        end
+        @todo = Todo.find_by_id(params[:id])
+    end
+    def new
+    end
+    def create
+        t=Todo.new
+        t.description=params["description"]
+        t.Pomodoro_estimate=params["Pomodoro_estimate"]
+        t.save
+        redirect_to "/todo/show/#{t.id}"
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
 get "todo/index", to: "todo#index"
+get "todo/new", to: "todo#new"
+get "todo/create", to: "todo#create"
 get "todo/show/:id", to: "todo#show"
+get "letter/index", to: "letter#index"
 end
