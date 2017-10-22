41cc29a0e6eb53c623366cce1f78571863756060
Joshua Velazquez <joshua.velazquez@epicacademy.org>
2017-10-22 19:05:46 +0000

    todo_backend

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -1,5 +1,6 @@
 class TodoController <ApplicationController
     def index
+        @todo=Todo.all
     end
     def show
         @todo = Todo.find_by_id(params[:id])
@@ -12,5 +13,20 @@ class TodoController <ApplicationController
         t.Pomodoro_estimate=params["Pomodoro_estimate"]
         t.save
         redirect_to "/todo/show/#{t.id}"
+    end
+    def edit
+        @todo = Todo.find_by_id(params[:id])
+    end
+    def update
+        t=Todo.find_by_id(params["id"])
+        t.description=params["description"]
+        t.Pomodoro_estimate=params["pomodoro_estimate"]
+        t.save
+        redirect_to "/todo/show/#{t.id}"
+    end
+    def destroy
+        t=Todo.find_by_id(params["id"])
+        t.destroy
+        redirect_to "/todo/index"
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
 get "todo/index", to: "todo#index"
 get "todo/new", to: "todo#new"
 get "todo/create", to: "todo#create"
+get "todo/edit/:id", to: "todo#edit"
+get "todo/update/:id", to: "todo#update"
+get "todo/destroy/:id", to: "todo#destroy"
 get "todo/show/:id", to: "todo#show"
 get "letter/index", to: "letter#index"
 end
